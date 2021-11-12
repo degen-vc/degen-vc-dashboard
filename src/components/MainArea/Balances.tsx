12d9074b3cbc7f0e55502aa98067b8b0dbb93822
Elena Derymova <elenaderymova@gmail.com>
2021-11-12 09:22:19 +0200

    Wallet connect added, balances WIP

--- src/components/MainArea/Balances.tsx
+++ src/components/MainArea/Balances.tsx
@@ -1,4 +1,8 @@
-import { HStack, Text, Spacer, Box, Heading } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+import { HStack, Text, Spacer, Box, Heading, Button } from "@chakra-ui/react";
+import { Contract, ethers, BigNumber } from "ethers";
+import ConnectWallet, { targetNetwork } from "./ConnectWallet";
+import { Web3Provider, Signer } from "../../types";
 
 const TokenBalance = ({
   tokenName,
@@ -15,13 +19,52 @@ const TokenBalance = ({
 );
 
 function Balances() {
+  const { dgvcAddress } = targetNetwork;
+
+  const DGVC2ABI = require("../../abi/DGVC2.json");
+
+  const [dgvcToken, setDgvcToken] = useState<Contract>();
+  const [dgvcBalance, setDgvcBalance] = useState<BigNumber>(BigNumber.from(0));
+  const [provider, setProvider] = useState<Web3Provider>();
+  const [signer, setSigner] = useState<Signer>();
+  const [signerAddress, setSignerAddress] = useState<string>();
+
+  useEffect(() => {
+    if (provider) {
+      setDgvcToken(new ethers.Contract(dgvcAddress, DGVC2ABI, provider));
+      setSigner(provider.getSigner(0));
+      
+    }
+  }, [provider]);
+
+  // Get signer address
+  useEffect(() => {
+    const getSignerAddress = async () => {
+      if (signer) {
+        setSignerAddress(await signer.getAddress());
+      }
+    };
+
+    getSignerAddress();
+  }, [signer]);
+
+  useEffect(() => {
+    if (signerAddress) {
+      fetchUserBalance();
+    }
+  }, [signerAddress]);
+
+  const fetchUserBalance = async () => {
+    setDgvcBalance(await dgvcToken!.balanceOf(signerAddress));
+  };
+
   return (
     <Box pt={{ base: "1.5rem", md: "2rem" }}>
       <Box border="1.5px solid white" p="1.5rem" px="2rem" rounded="xl">
         <Heading fontSize="2xl" pb="1rem">
           Balances
         </Heading>
-        <TokenBalance tokenName="DGVC" balance="XX" />
+        <TokenBalance tokenName="DGVC" balance={dgvcBalance.toString()} />
         <TokenBalance tokenName="DGVC LP" balance="XX" />
         <TokenBalance tokenName="Matic" balance="XX" />
         <TokenBalance tokenName="Usdc" balance="XX" />
--- src/components/MainArea/TopLinks.tsx
+++ src/components/MainArea/TopLinks.tsx
@@ -1,6 +1,31 @@
-import { Flex, Text, Link, HStack } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+
+import { Flex, Text, Link, HStack, Box} from "@chakra-ui/react";
+import ConnectWallet, { targetNetwork } from "./ConnectWallet";
+import { Web3Provider, Signer } from "../../types";
 
 function TopLinks() {
+  const [provider, setProvider] = useState<Web3Provider>();
+  const [signer, setSigner] = useState<Signer>();
+  const [signerAddress, setSignerAddress] = useState<string>();
+
+  useEffect(() => {
+    if (provider) {
+      setSigner(provider.getSigner(0));
+    }
+  }, [provider]);
+
+  // Get signer address
+  useEffect(() => {
+    const getSignerAddress = async () => {
+      if (signer) {
+        setSignerAddress(await signer.getAddress());
+      }
+    };
+
+    getSignerAddress();
+  }, [signer]);
+
   return (
     <Flex direction={{ base: "column", md: "row" }}>
       <Text>Degen VC: Allocate to venture -&nbsp;</Text>
